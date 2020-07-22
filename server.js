f4fb7e73f753a37687b0b53dd36b972f5ab43946
amardito <amardito37@gmail.com>
2020-07-22 11:07:08 +0700

    server exec

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-const http = require('http');
 const app = require('./app');
 
 const port = process.env.port || 3000;
 
-const server = http.createServer(app);
-
-server.listen(port);
+app.listen(port);
