8689f7835d5f487a6903d425748cbe61163bc8e6
amardito <amardito37@gmail.com>
2020-07-23 15:15:33 +0700

    patch for cpromo

--- API/routes/createpromo.js
+++ API/routes/createpromo.js
@@ -2,15 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Special = require('../models/special');
 
-//show all promos created
-router.get('/', async (req, res, next) => {
-    await Special.find()
-    .select("idToko promoName requireItem bonusItem promoPrice")
-    .then(doc => {
-        res.status(200).json(doc);
-    });
-});
-
 //create a promos
 router.post('/', async (req, res, next) => {
     const special = new Special({
@@ -27,26 +18,12 @@ router.post('/', async (req, res, next) => {
     });
 });
 
-//search for specific promos
-router.get('/:cpromo', async (req, res, next) => {
-    await Special.findById({_id : req.params.cpromo})
-    .then(doc => {
-        if(doc){
-            res.status(200).json(doc);
-        }else{
-            res.status(404).json({
-                error : 'Not Found'
-            });
-        }
-    });
-});
-
-
 //update some specific promos
 router.put('/:cpromo', async (req, res, next) => {
     await Special.updateOne({_id : req.params.cpromo}, {$set : req.body.data})
     res.status(200).send({updating : req.body.data});
 });
+
 //delete specific promos
 router.delete('/:cpromo', async (req, res, next) => {
    await Special.remove({_id : req.params.cpromo})
